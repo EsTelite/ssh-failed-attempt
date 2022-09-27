966a4fffadb1f97ed40b98f225dc9d189e7615e8
Riski Cahyadi <riskicahyadi@gmail.com>
2022-09-28 01:12:58 +0700

    add vagrant and util

--- producer/dbconn.py
+++ producer/dbconn.py
@@ -1,6 +1,6 @@
 import os
 from pymongo import MongoClient
 
-os.environ["MONGODB_URI"] = "mongodb://root:labcepatsaji777@192.168.241.10:27017" # :)
+os.environ["MONGODB_URI"] = "mongodb://root:labcepatsaji777@localhost:27017" # TODO: Need to utilize env var or secret engine for real
 client = MongoClient(os.environ['MONGODB_URI'])
 db = client['attempts']
